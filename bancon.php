5b64fe08a86c7b846b800adf511eed18db33d3ef
Shyam Kalariya <shyamkalariya@gmail.com>
2017-05-17 12:20:17 +0530

    Update bancon.php

--- bancon.php
+++ bancon.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>BANQUET and CONFERENCE</title>
+	<title>restaurant</title>
 	<link rel="stylesheet" type="text/css" href="link.html">
 </head>
 <body bgcolor="#fff2e5">
@@ -8,85 +8,58 @@
 	include "index.php";
 ?>
 <br><br>
-<center><font color=choco size=4><b><i><u>BANQUET & CONFERENCE</u></i></b></font></center>
+<center><font color=choco size=4><b><i><u>RESTAURANT</u></i></b></font></center>
 <br><br>
-<center><font color="#7c0000" size=4>Banquet:Celebration and Conference:Crystal</font></center>
-<table>
+<center><font color="#7c0000" size=4>Restaurant:Sessions</font></center>
+<table width=100%>
 <tr>
-	<td><a href=b1.php target=_blank><img src="image/hmarriage.jpg" width=125 height=150></a></td>
-	<td>The hotel offers facelities for meetings
-		every occasion whether for a conference, business seminar,business associates over lunch or dinner.</td>
-	<td><a href=b2.php target=_blank><img src="image/hmeetings.jpg" width=125 height=150></a></td>
-</tr>
-<tr>
-	<td><font color="#7c0000">Celebration</font></td>
+	<td><a href=r1.php target=_blank><img src="image/dining3.jpg" width=125 height=150></a></td>
 	<td></td>
-	<td><font color="#7c0000">U-shape</font></td>
+	<td><a href=r2.php target=_blank><img src="image/dining2.jpg" width=125 height=150></a></td>
 </tr>
 <tr>
-	<td>&nbsp;</td>
-	<td>The hotel Banquet parties accomodates 120 persons in a cozy atmosphere
-	and marriage parties are arranged in our pool garden </td>
-	<td>&nbsp;</td>
-</tr>
-<tr>
-	<td><a href=b3.php target=_blank><img src="image/meeting2.jpg" width=125 height=150></a></td>
-	<td>Our proffessional & exprienced staff shell be always available to assist you to the last moment in helping
-	you enduting success in any banquet & meetings.</td>
-	<td><a href=b4.php target=_blank><img src="image/meeting1.jpg" width=125 height=150></a></td>
-</tr>
-<tr>
-	<td><font color="#7c0000">Theatre style</font></td>
+	<td><font color="#7c0000">Entry</font></td>
 	<td></td>
-	<td><font color="#7c0000">Causule</font></td>
+	<td><font color="#7c0000">Palm</font></td>
 </tr>
-<table>
-<br><br>
-<table border=1 align=center>
+
 <tr>
-	<th>SITTING STYLE</th>
-	<th>SITTING CAP.</th>
-</tr>
-<tr>
-	<td>U-SHAPE</td>
-	<td>35</td>
+	<td><a href=r3.php target=_blank><img src="image/dining1.jpg" width='125' height='150'></a></td>
+	<td></td>
+	<td><a href=r4.php target=_blank><img src="image/delicacy.jpg" width='125' height='150'></a></td>
 </tr>
 <tr>
-	<td>CLASS ROOM STYLE</td>
-	<td>45</td>
+	<td><font color="#7c0000">Sitting style</font></td>
+	<td></td>
+	<td><font color="#7c0000">Cuisine</font></td>
 </tr>
 <tr>
-	<td>THEATRE STYLE</td>
-	<td>120</td>
+	<td></td>
+	<td><font color="#7c0000"><b>TIMING:</b></td>
+	<td></td>
+</tr>
+
+<tr>
+	<td></td>
+	<td>Lunch :12.00 to 15.30 hours</td>
+	<td></td>
 </tr>
 <tr>
-	<td>CASULE</td>
-	<td>50</td>
+<td></td>
+	<td>Dinner :19.00 to 23.30 hours</td>
+	<td></td>
 </tr>
+
 </table>
-<table>
-<tr>
-	<td>
-	<ul type=square>
-	<font size=4 color="#7c0465"><li>Facilities :</font>
-	<ul type=disc>
-	<font color=red>
-	<li>LCD multimedia projector
-	<li>Computer system
-	<li>Internet access
-	<li>White board
-	<li>Conference kits
-	<li>DJ system
-	<li>DVD,VCD,CD player
-	<li>Telephone
-	</ul>
-	</ul>
-	</td>
-</tr>
-</table>
+<p><br>
+  <br>
+    <font color="#7c0000" size=5>Cuisine   :</font>
+  <br>
+  <br>
+    <font color=darkpink size=4>Indian/Chiness/Mexican/Continetal</font></p>
+
 </body>
 </html>
-	
 	
 	
 	
